f7032bc58b2c24cc076b8eb050fa6ca83af6dfd9
nirmalpol <33207203+nirmalpol@users.noreply.github.com>
2018-07-18 15:17:23 +0800

    Update ADCErrorTest.py

--- ADCErrorTest.py
+++ ADCErrorTest.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 #MAX11100 Software Program
+'''
+File Name: ADCErrorTest.py
+Authors: Nirmal Pol and Raghav Srinivasan
+Date: July 18, 2018
+
+Description: This code allows the user to plot the error that is produced by the MAX11100 ADC. It also provides the user with the
+standard deviation and an equation that models the error. This way, the user can adjust the output of the MAX11100 to remove the 
+error that is produced.
+
+Note: The Raspberry Pi is connected to the AD5760 DAC which uses the AD5760.py file. The AD5760's output voltage is connected to
+the MAX11100 ADC input which utilizes the MAX11100.py file. Finally, the output of the MAX111000's output digital signal is 
+connected back to the Raspberry Pi for reading and producing the graph.
+'''
+
 
 import time, math
 import RPi.GPIO as GPIO
@@ -21,8 +35,10 @@ x = []
 y = []
 e = []
 a = 0
-actual = 0
+actual = 0            #Start at 0V
 for i in range(0, 100):
+        #Configure the DAC to output voltage values that start at 0V and increment by 1V until it reaches 10V.
+        
         dac.setVoltage(int(a))
         dac.update()
         time.sleep(0.05)
@@ -30,10 +46,12 @@ for i in range(0, 100):
         print (val)
         x = x + [actual]
         y = y + [val]
-        e = e + [abs(val - actual)]
-        actual = (actual + 1)%11
-        a = actual*(6553.5)
-                        
+        e = e + [abs(val - actual)]            #Calculate the error
+        actual = (actual + 1)%11               #Increment the voltage by 1V
+        a = actual*(6553.5)     
+
+#Plot and produce an equation along with the standard deviation.
+
 GPIO.cleanup()
 plt.plot(x, e, 'ro')
 plt.xlabel("DAC Voltage (V)")
