e8cb3b6e60477bd4c4ec89aa5763266437962855
Nirmal Pol <33207203+nirmalpol@users.noreply.github.com>
2018-10-05 18:44:40 -0400

    Add files via upload

--- Nanoimprinter Code July 19, 2018/nanoimprinter type 2 14-7/MAX11100.py
+++ Nanoimprinter Code July 19, 2018/nanoimprinter type 2 14-7/MAX11100.py
@@ -22,6 +22,9 @@ import RPi.GPIO as GPIO
 
 class max11100(object):
     def __init__(self, csPin = 7, misoPin = 9, clkPin = 11):
+        ''' Initializes the <MAX11100 object and sets all the parameters and GPIO values
+        '''
+        
         self.csPin = csPin
         self.misoPin = misoPin
         self.clkPin = clkPin
@@ -35,7 +38,31 @@ class max11100(object):
         GPIO.output(self.csPin, GPIO.HIGH)
         GPIO.output(self.clkPin, GPIO.LOW)
 
+    def readVoltage(self):
+        '''
+            The MAX11100 outputs the first 8 bits as leading zeros with the following
+            8 bits and the last 8 bits containing the 16 bits of data
+        '''
+        
+        GPIO.output(self.csPin, GPIO.LOW)
+        leadzeros = self.recvByte()
+        value1 = self.recvByte()
+        value2 = self.recvByte()
+        GPIO.output(self.csPin, GPIO.HIGH)
+        time.sleep(0.01)
+        rval = (value1 << 8) | value2
+        
+        #Convert to decimal:
+        volt = (rval*10.0)/65535
+        volt = (volt - 0.0001)/0.97675 #Fudge factors as ADC is slightly off
+        return volt
+    
     def recvByte(self):
+        '''
+            This method causes the MISO pin to turn 1 or 0 at each iteraation of the loop. It 
+            writes one byte of data to the DAC
+        '''
+        
         byte = 0x00
         for bit in range(8):
             GPIO.output(self.clkPin, GPIO.HIGH)
@@ -44,18 +71,3 @@ class max11100(object):
                 byte |= 0x1
             GPIO.output(self.clkPin, GPIO.LOW)
         return byte
-
-    def readVoltage(self):
-        '''The ADC outputs the first 8 bits as leading zeros with the following
-        8 bits and the last 8 bits containing the 16 bits of data'''
-        GPIO.output(self.csPin, GPIO.LOW)
-        leadzeros = self.recvByte()
-        value1 = self.recvByte()
-        value2 = self.recvByte()
-        GPIO.output(self.csPin, GPIO.HIGH)
-        time.sleep(0.01)
-        rval = (value1 << 8) | value2
-        #Convert to decimal:
-        volt = (rval*10.0)/65535
-        return volt
-        
